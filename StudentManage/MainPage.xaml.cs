00066469912ad8d0922ebd9d1606931d71027e26
nguyentient2009a <tienndth2008051@fpt.edu.vn>
2021-10-07 17:43:42 +0700

    asm

---
+++ StudentManage/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using StudentManage.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace StudentManage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Student> ListStudent;
+        private List<Avatar> Avatars;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ListStudent = StudentManager.GetListStudents();
+            Avatars = ItemAvatar.GetAvatars();
+        }
+
+        private void NewContactButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            ListStudent.Add(new Student { Count = ListStudent.Count + 1, FullName = FullName.Text, BirthDay = BirthDay.Text, Avatar = ((Avatar)AvatarComboBox.SelectedValue).AvatarImage });
+        }
+    }
+}
---
+++ StudentManage/Model/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManage.Model
+{
+    public class Student
+    {
+        public int Count { get; set; }
+        public string FullName { get; set; }
+        public string BirthDay { get; set; }
+        public string Avatar { get; set; }
+    }
+    public class StudentManager
+    {
+        public static ObservableCollection<Student> GetListStudents()
+        {
+            var students = new ObservableCollection<Student>();
+            return students;
+        }
+    }
+    public class Avatar
+    {
+        public string AvatarImage { get; set; }
+    }
+    public class ItemAvatar
+    {
+        public static List<Avatar> GetAvatars()
+        {
+
+            var avatars = new List<Avatar>();
+            avatars.Add(new Avatar { AvatarImage = "Assets/avatar.png" });
+            avatars.Add(new Avatar { AvatarImage = "Assets/avatar2.png" });
+            avatars.Add(new Avatar { AvatarImage = "Assets/avatar3.png" });
+            return avatars;
+        }
+    }
+
+  
+}
